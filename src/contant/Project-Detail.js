bb59b314f79df47f0df26e318d69c09e8b684c82
praveen <fub141999@gmail.com>
2024-08-02 00:35:37 +0530

    change the link of contant

--- src/contant/Project-Detail.js
+++ src/contant/Project-Detail.js
@@ -2,21 +2,21 @@ import project1 from "../assets/cnc2d.png"
 import project2 from "../assets/website-blog.jpg"
 export const PROJECT_LIST = [
     {
-        liveDemo:"https://www.linkedin.com/in/praveen-s-luci/",
+        liveDemo:"https://projecthub.arduino.cc/Mrinnovative/arduino-based-mini-cnc-2d-plotter-796c2f",
         title : "CNC mini 2D printer",
         image: project1 ,
         description : 
             "It is an IoT project and we did it with the Arduino-Uno, CD-ROM, stepper motor, and some electronic components we use programming languages like c to code the device and  its use for  mini craft, prototyping, Educational and designing purposes",
         technologies : ["Arduino-IDE"],
-        github: "https://github.com/Praveen593/IMDB_Scores"
+        github:"https://github.com/Praveen593"
     },
     {
-        liveDemo:"https://www.linkedin.com/in/praveen-s-luci/",
+        liveDemo:"",
         title: "API integration",
         image: project2,
         description: "In Progress",
         technologies : ["nodejs " ,"reactjs ","mongo db" ],
-        github: "https://github.com/Praveen593/IMDB_Scores"
+        github: "https://github.com/Praveen593"
     }
 
 ]
